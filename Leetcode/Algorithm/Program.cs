5525d84610daf41aa7be60f3f881148a992fdf59
spriteboysz <sprite_bx@126.com>
2023-03-18 10:33:34 +0800

    2023031810

---
+++ Leetcode/Algorithm/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Algorithm {
+    internal class Program {
+        public static void Main(string[] args)
+        {
+            P0001.Test();
+            P0002.Test();
+            
+        }
+    }
+}
---
+++ Leetcode/lib/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace lib {
+    public class Program {
+        public static void Test()
+        {
+            Console.WriteLine("lib.program.test");
+            Console.ReadLine();
+        }
+        public static void Main()
+        {
+            Test();
+            Console.WriteLine("lib.progrom");
+            Console.ReadLine();
+        }
+    }
+}
