cacf42d2fca2171b300cc4c277e573fc45e998e4
spriteboysz <sprite_bx@126.com>
2023-04-01 21:15:14 +0800

    2023040121

---
+++ Leetcode/Algorithm/P1295.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Author: Deean
+ * Date: 2023-03-31 22:02:03
+ * FileName: P1295.cs
+ * Description:
+*/
+
+using System;
+
+namespace Algorithm;
+
+public class P1295 {
+    public class Solution {
+        public int FindNumbers(int[] nums) {
+            int count = 0;
+            foreach (var num in nums) {
+                int cnt = 0, cur = num;
+                while (cur > 0) {
+                    cur /= 10;
+                    cnt++;
+                }
+                if (cnt % 2 == 0) count++;
+            }
+            return count;
+        }
+    }
+
+    public static void Test() {
+        var s = new Solution();
+        var ans = s.FindNumbers(new[] { 12, 345, 2, 6, 7896 });
+        Console.WriteLine(ans);
+    }
+}
---
+++ Leetcode/Algorithm/P1299.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Author: Deean
+ * Date: 2023-03-31 21:54:52
+ * FileName: P1299.cs
+ * Description:
+*/
+
+using System;
+
+namespace Algorithm; 
+
+public class P1299 {
+    public class Solution {
+        public int[] ReplaceElements(int[] arr) {
+            int maximum = -1, n = arr.Length;
+            for (int i = n - 1; i >= 0; i--) {
+                int cur = arr[i];
+                arr[i] = maximum;
+                maximum = Math.Max(maximum, cur);
+            }
+            return arr;
+        } 
+    }
+
+    public static void Test() {
+        var s = new Solution();
+        var ans = s.ReplaceElements(new[]{17,18,5,4,6,1});
+        Console.WriteLine(lib.Arrays.ToString(ans));
+    }
+}
