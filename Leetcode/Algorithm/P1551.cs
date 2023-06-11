c80a85e8bed4eac42d331dc7afe4ecca3994df89
spriteboysz <sprite_bx@126.com>
2023-06-11 17:06:20 +0800

    2023061117

---
+++ Leetcode/Algorithm/P1551.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Author: Deean
+ * Date: 2023-06-10 23:10:01
+ * FileName: P1551.cs
+ * Description:
+*/
+
+using System;
+
+namespace Algorithm; 
+
+public class P1551 {
+    public class Solution {
+        public int MinOperations(int n) {
+            return n * n / 4;
+        } 
+    }
+
+    public static void Test() {
+        var s = new Solution();
+        var ans = s.MinOperations(6);
+        Console.WriteLine(ans);
+    }
+}
