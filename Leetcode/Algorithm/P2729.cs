e907b22081cb9e5b009d6188cfbedc5f6397765f
spriteboysz <sprite_bx@126.com>
2023-08-17 22:49:34 +0800

    2023081722

---
+++ Leetcode/Algorithm/P2729.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Author: Deean
+ * Date: 2023-08-16 23:15:59
+ * FileName: P2729.cs
+ * Description:2729. 判断一个数是否迷人
+ */
+
+using System;
+using System.Text;
+
+namespace Algorithm;
+
+public class P2729 {
+    public class Solution {
+        public bool IsFascinating(int n) {
+            StringBuilder ss = new StringBuilder();
+            for (int i = 1; i <= 3; i++) {
+                ss.Append((n * i).ToString());
+            }
+            int[] digits = new int[10];
+            foreach (var c in ss.ToString()) {
+                digits[c - '0']++;
+            }
+            for (int i = 1; i < 10; i++) {
+                if (digits[i] != 1) {
+                    return false;
+                }
+            }
+            return digits[0] == 0;
+        }
+    }
+
+    public static void Test() {
+        var s = new Solution();
+        var ans = s.IsFascinating(192);
+        Console.WriteLine(ans);
+    }
+}
---
+++ Leetcode/Algorithm/P2733.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Author: Deean
+ * Date: 2023-08-16 23:04:55
+ * FileName: P2733.cs
+ * Description:2733. 既不是最小值也不是最大值
+ */
+
+using System;
+using System.Linq;
+
+namespace Algorithm;
+
+public class P2733 {
+    public class Solution {
+        public int FindNonMinOrMax(int[] nums) {
+            int maximum = nums.Max(), minimum = nums.Min();
+            foreach (var num in nums) {
+                if (num != maximum && num != minimum) {
+                    return num;
+                }
+            }
+            return -1;
+        }
+    }
+
+    public static void Test() {
+        var s = new Solution();
+        var ans = s.FindNonMinOrMax(new[] { 3, 2, 1, 4 });
+        Console.WriteLine(ans);
+    }
+}
