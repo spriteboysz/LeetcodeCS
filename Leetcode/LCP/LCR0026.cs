f573daa6f50ca8f48c66deb505b6da8c45fe364d
spriteboysz <sprite_bx@126.com>
2023-10-23 21:43:26 +0800

    2023102321

---
+++ Leetcode/LCP/LCR0026.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Author: Deean
+ * Date: 2023-10-21 14:41:04
+ * FileName: LCR0026.cs
+ * Description: LCR 026. 重排链表
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using lib;
+
+namespace LCP;
+
+public class LCR0026 {
+    public class Solution {
+        public void ReorderList(ListNode head) {
+            IList<ListNode> nodes1 = new List<ListNode>();
+            ListNode cur = head;
+            while (cur != null) {
+                nodes1.Add(cur);
+                cur = cur.next;
+            }
+            IList<ListNode> nodes2 = new List<ListNode>();
+            for (int i = 0, n = nodes1.Count; i < (n + 1) / 2; i++) {
+                nodes2.Add(nodes1[i]);
+                nodes2.Add(nodes1[n - 1 - i]);
+            }
+            for (int i = 0, n = nodes1.Count; i < n - 1; i++) {
+                nodes2[i].next = nodes2[i + 1];
+            }
+            nodes2.Last().next = null;
+            Console.WriteLine(head.ToString());
+        }
+    }
+
+    public static void Test() {
+        var s = new Solution();
+        var head = new ListNode("[1,2,3,4]");
+        s.ReorderList(head);
+    }
+}
---
+++ Leetcode/LCP/LCR0027.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Author: Deean
+ * Date: 2023-10-21 14:43:59
+ * FileName: LCR0027.cs
+ * Description: LCR 027. 回文链表
+ */
+
+using System;
+using System.Collections.Generic;
+using lib;
+
+namespace LCP; 
+
+public class LCR0027 {
+    public class Solution {
+        public bool IsPalindrome(ListNode head) {
+            if (head == null) return true;
+            IList<int> values = new List<int>();
+            while (head != null) {
+                values.Add(head.val);
+                head = head.next;
+            }
+            for (int i = 0, n = values.Count; i < n / 2; i++) {
+                if (values[i] != values[n - 1 - i]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    public static void Test() {
+        var s = new Solution();
+        var head = new ListNode("[1,2,3,3,2,1]");
+        var ans = s.IsPalindrome(head);
+        Console.WriteLine(ans);
+    }
+}
