a5dbdca48754b92f53b9b0f7df0d7de81d0eb044
spriteboysz <sprite_bx@126.com>
2023-10-28 21:03:42 +0800

    2023102821

---
+++ Leetcode/LCP/LCR0060.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * Author: Deean
+ * Date: 2023-10-23 22:35:32
+ * FileName: LCR0060.cs
+ * Description: LCR 060. 前 K 个高频元素
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using lib;
+
+namespace LCP;
+
+public class LCR0060 {
+    public class Solution {
+        public int[] TopKFrequent(int[] nums, int k) {
+            IDictionary<int, int> dict = new Dictionary<int, int>();
+            foreach (var num in nums) {
+                if (!dict.ContainsKey(num)) {
+                    dict[num] = 0;
+                }
+                dict[num] += 1;
+            }
+            int[] keys = dict.Keys.ToArray();
+            Array.Sort(keys, (a, b) => dict[b] - dict[a]);
+            IList<int> top = new List<int>();
+            for (int i = 0; i < k; i++) {
+                top.Add(keys[i]);
+            }
+            return top.ToArray();
+        }
+    }
+
+    public static void Test() {
+        var s = new Solution();
+        var ans = s.TopKFrequent(new[] { 1, 1, 1, 2, 2, 3 }, 2);
+        Console.WriteLine(Arrays.ToString(ans));
+    }
+}
---
+++ Leetcode/LCP/LCR0063.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Author: Deean
+ * Date: 2023-10-23 22:37:51
+ * FileName: LCR0063.cs
+ * Description: LCR 063. 单词替换
+ */
+
+using System;
+using System.Collections.Generic;
+
+namespace LCP;
+
+public class LCR0063 {
+    public class Solution {
+        public string ReplaceWords(IList<string> dictionary, string sentence) {
+            HashSet<string> set = new HashSet<string>();
+            foreach (var word in dictionary) {
+                set.Add(word);
+            }
+            string[] words = sentence.Split(' ');
+            for (int i = 0, n = words.Length; i < n; i++) {
+                for (int j = 0; j < words[i].Length; j++) {
+                    if (set.Contains(words[i].Substring(0, 1 + j))) {
+                        words[i] = words[i].Substring(0, 1 + j);
+                        break;
+                    }
+                }
+            }
+            return string.Join(" ", words);
+        }
+    }
+
+    public static void Test() {
+        var s = new Solution();
+        var ans = s.ReplaceWords(new[] { "cat", "bat", "rat" }, "the cattle was rattled by the battery");
+        Console.WriteLine(ans);
+    }
+}
