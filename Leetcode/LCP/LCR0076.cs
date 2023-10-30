9094eaea8e6975aa8427920a97c8dea8b53ad342
spriteboysz <sprite_bx@126.com>
2023-10-30 22:43:55 +0800

    2023103022

---
+++ Leetcode/LCP/LCR0076.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Author: Deean
+ * Date: 2023-10-26 00:00:56
+ * FileName: LCR0076.cs
+ * Description: LCR 076. 数组中的第 K 个最大元素
+ */
+
+using System;
+
+namespace LCP;
+
+public class LCR0076 {
+    public class Solution {
+        public int FindKthLargest(int[] nums, int k) {
+            Array.Sort(nums, (a, b) => b - a);
+            return nums[k - 1];
+        }
+    }
+
+    public static void Test() {
+        var s = new Solution();
+        var ans = s.FindKthLargest(new[] { 3, 2, 3, 1, 2, 4, 5, 5, 6 }, 4);
+        Console.WriteLine(ans);
+    }
+}
---
+++ Leetcode/LCP/LCR0077.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Author: Deean
+ * Date: 2023-10-26 22:40:57
+ * FileName: LCR0077.cs
+ * Description: LCR 077. 排序链表
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using lib;
+
+namespace LCP;
+
+public class LCR0077 {
+    public class Solution {
+        public ListNode SortList(ListNode head) {
+            if (head == null) return null;
+            IList<ListNode> nodes = new List<ListNode>();
+            while (head != null) {
+                nodes.Add(head);
+                head = head.next;
+            }
+            ListNode[] nodes2 = nodes.ToArray();
+            Array.Sort(nodes2, (n1, n2) => n1.val - n2.val);
+            for (int i = 0, n = nodes2.Length; i < n - 1; i++) {
+                nodes2[i].next = nodes2[i + 1];
+            }
+            nodes2.Last().next = null;
+            return nodes2.First();
+        }
+    }
+
+    public static void Test() {
+        var s = new Solution();
+        var ans = s.SortList(new ListNode("[5,4,3,2,1]"));
+        Console.WriteLine(ans);
+    }
+}
