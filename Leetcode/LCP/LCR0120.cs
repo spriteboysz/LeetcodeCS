4c748dbe3142fe1e0bf82d013df4f0f677eab91d
spriteboysz <sprite_bx@126.com>
2023-11-12 09:31:17 +0800

    2023111209

---
+++ Leetcode/LCP/LCR0120.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Author: Deean
+ * Date: 2023-11-11 09:54:13
+ * FileName: LCR0120.cs
+ * Description: LCR 120. 寻找文件副本
+ */
+
+using System;
+using System.Collections.Generic;
+
+namespace LCP;
+
+public class LCR0120 {
+    public class Solution {
+        public int FindRepeatDocument(int[] documents) {
+            HashSet<int> seen = new HashSet<int>();
+            foreach (var document in documents) {
+                if (seen.Contains(document)) {
+                    return document;
+                }
+                seen.Add(document);
+            }
+            return -1;
+        }
+    }
+
+    public static void Test() {
+        var s = new Solution();
+        var ans = s.FindRepeatDocument(new[] { 2, 5, 3, 0, 5, 0 });
+        Console.WriteLine(ans);
+    }
+}
---
+++ Leetcode/LCP/LCR0122.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Author: Deean
+ * Date: 2023-11-12 09:30:20
+ * FileName: LCR0122.cs
+ * Description: LCR 122. 路径加密
+ */
+
+using System;
+
+namespace LCP;
+
+public class LCR0122 {
+    public class Solution {
+        public string PathEncryption(string path) {
+            return path.Replace(".", " ");
+        }
+    }
+
+    public static void Test() {
+        var s = new Solution();
+        var ans = s.PathEncryption("a.aef.qerf.bb");
+        Console.WriteLine(ans);
+    }
+}
