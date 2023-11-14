882f2a592f3fd30216185f61029a1be8e9f136bf
spriteboysz <sprite_bx@126.com>
2023-11-14 22:48:21 +0800

    2023111422

---
+++ Leetcode/LCP/LCR0123.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * Author: Deean
+ * Date: 2023-11-13 22:57:47
+ * FileName: LCR0123.cs
+ * Description: LCR 123. 图书整理 I
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using lib;
+
+namespace LCP;
+
+public class LCR0123 {
+    public class Solution {
+        public int[] ReverseBookList(ListNode head) {
+            IList<int> values = new List<int>();
+            while (head != null) {
+                values.Add(head.val);
+                head = head.next;
+            }
+            int[] nums = values.ToArray();
+            Array.Reverse(nums);
+            return nums;
+        }
+    }
+
+    public static void Test() {
+        var s = new Solution();
+        var ans = s.ReverseBookList(new ListNode("[3,6,4,1]"));
+        Console.WriteLine(ans);
+    }
+}
---
+++ Leetcode/LCP/LCR0126.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Author: Deean
+ * Date: 2023-11-14 22:46:49
+ * FileName: LCR00126.cs
+ * Description: LCR 126. 斐波那契数
+ */
+
+using System;
+
+namespace LCP;
+
+public class LCR0126 {
+    public class Solution {
+        public int Fib(int n) {
+            const int MOD = 1000000007;
+            if (n <= 1) return n;
+            int p = 0, q = 0, r = 1;
+            for (int i = 2; i <= n; i++) {
+                p = q;
+                q = r;
+                r = (p + q) % MOD;
+            }
+            return r;
+        }
+    }
+
+    public static void Test() {
+        var s = new Solution();
+        var ans = s.Fib(2);
+        Console.WriteLine(ans);
+    }
+}
