e09968025fa0b679b434843f4f4c714abcf6b34b
spriteboysz <sprite_bx@126.com>
2023-11-16 22:51:43 +0800

    2023111622

---
+++ Leetcode/LCP/LCR0127.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Author: Deean
+ * Date: 2023-11-15 21:56:25
+ * FileName: LCR0127.cs
+ * Description: LCR 127. 跳跃训练
+ */
+
+using System;
+
+namespace LCP;
+
+public class LCR0127 {
+    public class Solution {
+        public int TrainWays(int n) {
+            const int MOD = 1000000007;
+            if (n <= 1) return 1;
+            int a = 1, b = 1, num = 0;
+            for (int i = 2; i <= n; i++) {
+                num = (a + b) % MOD;
+                a = b;
+                b = num;
+            }
+            return num;
+        }
+    }
+
+    public static void Test() {
+        var s = new Solution();
+        var ans = s.TrainWays(5);
+        Console.WriteLine(ans);
+    }
+}
---
+++ Leetcode/LCP/LCR0128.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Author: Deean
+ * Date: 2023-11-16 22:50:37
+ * FileName: LCR0128.cs
+ * Description: LCR 128. 库存管理 I
+ */
+
+using System;
+
+namespace LCP;
+
+public class LCR0128 {
+    public class Solution {
+        public int StockManagement(int[] stock) {
+            int minimum = stock[0];
+            for (int i = 1, n = stock.Length; i < n; i++) {
+                minimum = Math.Min(minimum, stock[i]);
+            }
+            return minimum;
+        }
+    }
+
+    public static void Test() {
+        var s = new Solution();
+        var ans = s.StockManagement(new [] { 4, 5, 8, 3, 4 });
+        Console.WriteLine(ans);
+    }
+}
