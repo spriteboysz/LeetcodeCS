d915ce633ac37726cb1da7f82e1edd5b56dbd80b
spriteboysz <sprite_bx@126.com>
2023-12-02 12:44:09 +0800

    2023120212

---
+++ Leetcode/LCP/LCR0153.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Author: Deean
+ * Date: 2023-12-01 22:03:47
+ * FileName: LCR0153.cs
+ * Description: LCR 153. 二叉树中和为目标值的路径
+ */
+
+using System;
+using System.Collections.Generic;
+using lib;
+
+namespace LCP;
+
+public class LCR0153 {
+    public class Solution {
+        private IList<IList<int>> paths;
+        private IList<int> path;
+
+        private void Backtrace(TreeNode root, int sum) {
+            if (root == null) return;
+            path.Add(root.val);
+            sum -= root.val;
+            if (sum == 0 && root.left == null && root.right == null) {
+                paths.Add(new List<int>(path));
+            }
+            Backtrace(root.left, sum);
+            Backtrace(root.right, sum);
+            path.RemoveAt(path.Count - 1);
+        }
+
+        public IList<IList<int>> PathTarget(TreeNode root, int target) {
+            paths = new List<IList<int>>();
+            path = new List<int>();
+            if (root == null) return paths;
+            Backtrace(root, target);
+            return paths;
+        }
+    }
+
+    public static void Test() {
+        var s = new Solution();
+        var ans = s.PathTarget(new TreeNode("[5,4,8,11,null,13,4,7,2,null,null,5,1]"), 22);
+        Console.WriteLine(ans);
+    }
+}
---
+++ Leetcode/LCP/LCR0154.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * Author: Deean
+ * Date: 2023-12-02 12:41:58
+ * FileName: LCR0154.cs
+ * Description: LCR 154. 复杂链表的复制
+ */
+
+using System;
+using System.Collections.Generic;
+
+namespace LCP;
+
+public class LCR0154 {
+    public class Solution {
+        public class Node {
+            public int val;
+            public Node next;
+            public Node random;
+
+            public Node(int _val) {
+                val = _val;
+                next = null;
+                random = null;
+            }
+        }
+
+        public Node CopyRandomList(Node head) {
+            if (head == null) return null;
+            IDictionary<Node, Node> dict = new Dictionary<Node, Node>();
+            Node cur = head;
+            while (cur != null) {
+                dict.Add(cur, new Node(cur.val));
+                cur = cur.next;
+            }
+            cur = head;
+            while (cur != null) {
+                if (cur.next != null) {
+                    dict[cur].next = dict[cur.next];
+                }
+                if (cur.random != null) {
+                    dict[cur].random = dict[cur.random];
+                }
+                cur = cur.next;
+            }
+            return dict[head];
+        }
+    }
+
+    public static void Test() {
+        var s = new Solution();
+        var ans = s.CopyRandomList(new Solution.Node(1));
+        Console.WriteLine(ans);
+    }
+}
