47f924ddd528a7e3ac0ca0d9422f537f28ad06c8
spriteboysz <sprite_bx@126.com>
2023-12-04 22:47:52 +0800

    2023120422

---
+++ Leetcode/LCP/LCR0157.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * Author: Deean
+ * Date: 2023-12-03 17:30:44
+ * FileName: LCR0157.cs
+ * Description: LCR 157. 套餐内商品的排列顺序
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LCP;
+
+public class LCR0157 {
+    public class Solution {
+        private IList<string> paths;
+        private StringBuilder path;
+        private bool[] visited;
+
+        private void Backtrace(char[] arr, int pos) {
+            if (pos == arr.Length) {
+                paths.Add(path.ToString());
+            }
+            for (int i = 0; i < arr.Length; i++) {
+                if (visited[i] || (i > 0 && arr[i - 1] == arr[i] && !visited[i - 1])) {
+                    continue;
+                }
+                visited[i] = true;
+                path.Append(arr[i]);
+                Backtrace(arr, pos + 1);
+                path.Length--;
+                visited[i] = false;
+            }
+        }
+
+        public string[] GoodsOrder(string goods) {
+            paths = new List<string>();
+            path = new StringBuilder();
+            visited = new bool[goods.Length];
+            char[] arr = goods.ToCharArray();
+            Array.Sort(arr);
+            Backtrace(arr, 0);
+            return paths.ToArray();
+        }
+    }
+
+    public static void Test() {
+        var s = new Solution();
+        var ans = s.GoodsOrder("agew");
+        Console.WriteLine(ans);
+    }
+}
---
+++ Leetcode/LCP/LCR0158.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * Author: Deean
+ * Date: 2023-12-04 22:46:41
+ * FileName: LCR0158.cs
+ * Description: LCR 158. 库存管理 II
+ */
+
+using System;
+using System.Collections.Generic;
+
+namespace LCP;
+
+public class LCR0158 {
+    public class Solution {
+        public int InventoryManagement(int[] stock) {
+            IDictionary<int, int> dict = new Dictionary<int, int>();
+            foreach (var num in stock) {
+                if (!dict.ContainsKey(num)) {
+                    dict.Add(num, 0);
+                }
+                if (++dict[num] > stock.Length / 2) {
+                    return num;
+                }
+            }
+            return -1;
+        }
+    }
+
+    public static void Test() {
+        var s = new Solution();
+        var ans = s.InventoryManagement(new[] { 6, 1, 3, 1, 1, 1 });
+        Console.WriteLine(ans);
+    }
+}
