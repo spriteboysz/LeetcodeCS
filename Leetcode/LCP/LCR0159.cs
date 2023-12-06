b91981b69882da2c1a690e6e10f330f49c7c31de
spriteboysz <sprite_bx@126.com>
2023-12-06 20:13:24 +0800

    2023120620

---
+++ Leetcode/LCP/LCR0159.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Author: Deean
+ * Date: 2023-12-05 22:55:25
+ * FileName: LCR0159.cs
+ * Description: LCR 159. 库存管理 III
+ */
+
+using System;
+
+namespace LCP;
+
+public class LCR0159 {
+    public class Solution {
+        public int[] InventoryManagement(int[] stock, int cnt) {
+            Array.Sort(stock);
+            int[] least = new int[cnt];
+            for (int i = 0; i < cnt; i++) {
+                least[i] = stock[i];
+            }
+            return least;
+        }
+    }
+
+    public static void Test() {
+        var s = new Solution();
+        var ans = s.InventoryManagement(new[] { 12, 5, 7, 4 }, 1);
+        Console.WriteLine(ans);
+    }
+}
---
+++ Leetcode/LCP/LCR0161.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Author: Deean
+ * Date: 2023-12-06 20:12:15
+ * FileName: LCR0161.cs
+ * Description: LCR 161. 连续天数的最高销售额
+ */
+
+using System;
+
+namespace LCP;
+
+public class LCR0161 {
+    public class Solution {
+        public int MaxSales(int[] sales) {
+            int acc = 0, maximum = sales[0];
+            foreach (var num in sales) {
+                acc = Math.Max(acc + num, num);
+                maximum = Math.Max(maximum, acc);
+            }
+            return maximum;
+        }
+    }
+
+    public static void Test() {
+        var s = new Solution();
+        var ans = s.MaxSales(new[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 });
+        Console.WriteLine(ans);
+    }
+}
