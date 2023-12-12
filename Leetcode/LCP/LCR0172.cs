dc5943bfaaa1df71854b8cf0d5574b8b44a7e180
spriteboysz <sprite_bx@126.com>
2023-12-12 22:50:06 +0800

    2023121222

---
+++ Leetcode/LCP/LCR0172.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Author: Deean
+ * Date: 2023-12-11 22:52:29
+ * FileName: LCR0172.cs
+ * Description: LCR 172. 统计目标成绩的出现次数
+ */
+
+using System;
+using System.Collections.Generic;
+
+namespace LCP;
+
+public class LCR0172 {
+    public class Solution {
+        public int CountTarget(int[] scores, int target) {
+            IDictionary<int, int> dict = new Dictionary<int, int>();
+            foreach (var score in scores) {
+                if (!dict.ContainsKey(score)) {
+                    dict.Add(score, 0);
+                }
+                dict[score]++;
+            }
+            return dict.ContainsKey(target) ? dict[target] : 0;
+        }
+    }
+
+    public static void Test() {
+        var s = new Solution();
+        var ans = s.CountTarget(new[] { 2, 2, 3, 4, 4, 4, 5, 6, 6, 8 }, 4);
+        Console.WriteLine(ans);
+    }
+}
---
+++ Leetcode/LCP/LCR0173.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Author: Deean
+ * Date: 2023-12-12 22:47:53
+ * FileName: LCR0173.cs
+ * Description: LCR 173. 点名
+ */
+
+using System;
+using System.Linq;
+
+namespace LCP;
+
+public class LCR0173 {
+    public class Solution {
+        public int TakeAttendance(int[] records) {
+            int n = records.Length;
+            return n * (n + 1) / 2 - records.Sum();
+        }
+    }
+
+    public static void Test() {
+        var s = new Solution();
+        var ans = s.TakeAttendance(new[] { 0, 1, 2, 3, 5 });
+        Console.WriteLine(ans);
+    }
+}
