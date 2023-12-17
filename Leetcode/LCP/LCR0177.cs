6706a10eed0cced31c00558de8ea816ed9ce75f7
spriteboysz <sprite_bx@126.com>
2023-12-17 10:50:49 +0800

    2023121710

---
+++ Leetcode/LCP/LCR0177.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * Author: Deean
+ * Date: 2023-12-16 21:48:50
+ * FileName: LCR0177.cs
+ * Description: LCR 177. 撞色搭配
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LCP;
+
+public class LCR0177 {
+    public class Solution {
+        public int[] SockCollocation(int[] sockets) {
+            IDictionary<int, int> dict = new Dictionary<int, int>();
+            foreach (var socket in sockets) {
+                if (!dict.ContainsKey(socket)) {
+                    dict.Add(socket, 0);
+                }
+                dict[socket]++;
+            }
+            IList<int> single = new List<int>();
+            foreach (var kv in dict) {
+                if (kv.Value == 1) single.Add(kv.Key);
+            }
+            return single.ToArray();
+        }
+    }
+
+    public static void Test() {
+        var s = new Solution();
+        var ans = s.SockCollocation(new[] { 4, 5, 2, 4, 6, 6 });
+        Console.WriteLine(ans);
+    }
+}
---
+++ Leetcode/LCP/LCR0178.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * Author: Deean
+ * Date: 2023-12-17 10:49:51
+ * FileName: LCR0178.cs
+ * Description: LCR 178. 训练计划 VI
+ */
+
+using System;
+using System.Collections.Generic;
+
+namespace LCP;
+
+public class LCR0178 {
+    public class Solution {
+        public int TrainingPlan(int[] actions) {
+            IDictionary<int, int> dict = new Dictionary<int, int>();
+            foreach (var action in actions) {
+                if (!dict.ContainsKey(action)) {
+                    dict.Add(action, 0);
+                }
+                dict[action]++;
+            }
+            foreach (var kv in dict) {
+                if (kv.Value == 1) return kv.Key;
+            }
+            return -1;
+        }
+    }
+
+    public static void Test() {
+        var s = new Solution();
+        var ans = s.TrainingPlan(new[] { 5, 7, 5, 5 });
+        Console.WriteLine(ans);
+    }
+}
