a1e4337c817ea08e8423ed2e450171b01aa60288
spriteboysz <sprite_bx@126.com>
2023-12-25 22:49:39 +0800

    2023122325

---
+++ Leetcode/LCP/LCR0188.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Author: Deean
+ * Date: 2023-12-24 22:48:14
+ * FileName: LCR0188.cs
+ * Description: LCR 188. 买卖芯片的最佳时机
+ */
+
+using System;
+
+namespace LCP;
+
+public class LCR0188 {
+    public class Solution {
+        public int BestTiming(int[] prices) {
+            int maximum = 0, minprice = Int32.MaxValue;
+            foreach (var price in prices) {
+                minprice = Math.Min(minprice, price);
+                maximum = Math.Max(maximum, price - minprice);
+            }
+            return maximum;
+        }
+    }
+    
+    public static void Test() {
+        var s = new Solution();
+        var ans = s.BestTiming(new[] { 3, 6, 2, 9, 8, 5 });
+        Console.WriteLine(ans);
+    }
+}
---
+++ Leetcode/LCP/LCR0189.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Author: Deean
+ * Date: 2023-12-25 22:48:30
+ * FileName: LCR0189.cs
+ * Description: LCR 189. 设计机械累加器
+ */
+
+using System;
+
+namespace LCP;
+
+public class LCR0189 {
+    public class Solution {
+        public int MechanicalAccumulator(int target) {
+            if (target == 1) return 1;
+            return MechanicalAccumulator(target - 1) + target;
+        }
+    }
+
+    public static void Test() {
+        var s = new Solution();
+        var ans = s.MechanicalAccumulator(5);
+        Console.WriteLine(ans);
+    }
+}
